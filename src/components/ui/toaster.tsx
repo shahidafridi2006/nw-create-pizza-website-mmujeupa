884594576e8399f8da093390ff10b4db2704fec6
shahidafridi2006 <shahidafridi424244@gmail.com>
2026-03-17 16:40:13 +0500

    feat: add src/components/ui/toaster.tsx [NovaWarm]

---
+++ src/components/ui/toaster.tsx
@@ -0,0 +1,40 @@
+import * as React from "react"
+import {
+  useToast,
+  toast,
+  type ToastProps,
+  type ToastActionElement,
+} from "@/components/ui/use-toast"
+import {
+  Toast,
+  ToastAction,
+  ToastClose,
+  ToastDescription,
+  ToastProvider,
+  ToastTitle,
+  ToastViewport,
+} from "@/components/ui/toast"
+
+export function Toaster() {
+  const { toasts } = useToast()
+
+  return (
+    <ToastProvider>
+      {toasts.map(function ({ id, title, description, action, ...props }) {
+        return (
+          <Toast key={id} {...props}>
+            <div className="grid gap-1">
+              {title && <ToastTitle>{title}</ToastTitle>}
+              {description && (
+                <ToastDescription>{description}</ToastDescription>
+              )}
+            </div>
+            {action}
+            <ToastClose />
+          </Toast>
+        )
+      })}
+      <ToastViewport />
+    </ToastProvider>
+  )
+}
